4b9bebc3565372ecff705e76d04743bb58a3acf4
lonewolf-0 <aryan.tyagi202@gmail.com>
2025-10-05 12:28:48 +0530

    refactor: improve code formatting and consistency in app.py

--- app.py
+++ app.py
@@ -8,9 +8,11 @@ CORS(app)
 
 API_URL = "https://api.allanime.day/api"
 
-HEADERS = {"Referer": "https://allanime.to",
-           "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/121.0",
-           "Content-Type": "application/json"}
+HEADERS = {
+    "Referer": "https://allanime.to",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/121.0",
+    "Content-Type": "application/json",
+}
 
 GRAPHQL_SEARCH_QUERY = """
 query(
@@ -45,19 +47,26 @@ query($showId: String!) {
 """
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
 def get_shows():
-    query = request.args.get('query', '').strip()
+    query = request.args.get("query", "").strip()
     if not query:
         return {"error": "Please enter a search term."}, 400
 
-    payload = {"query": GRAPHQL_SEARCH_QUERY,
-               "variables": {"search": {"allowAdult": False, "allowUnknown": False, "query": query}, "limit": 40,
-                             "page": 1, "translationType": "sub", "countryOrigin": "ALL"}}
+    payload = {
+        "query": GRAPHQL_SEARCH_QUERY,
+        "variables": {
+            "search": {"allowAdult": False, "allowUnknown": False, "query": query},
+            "limit": 40,
+            "page": 1,
+            "translationType": "sub",
+            "countryOrigin": "ALL",
+        },
+    }
 
     response = requests.post(API_URL, headers=HEADERS, json=payload)
     if response.status_code != 200:
@@ -70,13 +79,13 @@ def get_shows():
     return shows, query
 
 
-@app.route('/api/search', methods=['GET'])
+@app.route("/api/search", methods=["GET"])
 def api_search():
     shows, _q = get_shows()
     return {"shows": shows}
 
 
-@app.route('/search')
+@app.route("/search")
 def search():
     shows, _q = get_shows()
     # Limit 10 shows for display
@@ -103,7 +112,7 @@ def search():
     return render_template("results.html", shows=shows, query=_q)
 
 
-@app.route('/api/anime/episode', methods=['POST'])
+@app.route("/api/anime/episode", methods=["POST"])
 def anime_episode():
     data = request.json
     anime_id = data["anime_id"]
@@ -128,7 +137,7 @@ def anime_episode():
     name = show.get("name", "Unknown Anime")
     episode_data = show.get("availableEpisodesDetail", {})
     episodes = sorted(episode_data.get("sub", []), key=lambda x: float(x))
-    return {'episodes': episodes}
+    return {"episodes": episodes}
 
 
 def fetch_usable_urls(data):
@@ -148,10 +157,17 @@ def fetch_usable_urls(data):
         }
         """
 
-    payload = {"query": query, "variables": {"showId": anime_id, "translationType": "sub", "episodeString": ep_number}}
+    payload = {
+        "query": query,
+        "variables": {
+            "showId": anime_id,
+            "translationType": "sub",
+            "episodeString": ep_number,
+        },
+    }
 
-    if payload['variables']['translationType'] == 'sub' and lang == 'dub':
-        payload['variables']['translationType'] = 'dub'
+    if payload["variables"]["translationType"] == "sub" and lang == "dub":
+        payload["variables"]["translationType"] = "dub"
 
     response = requests.post(API_URL, headers=HEADERS, json=payload)
     if response.status_code != 200:
@@ -164,7 +180,7 @@ def fetch_usable_urls(data):
     for src in sources:
         raw_url = src.get("sourceUrl")
 
-        if raw_url.startswith('--'):
+        if raw_url.startswith("--"):
             cleaned_url = raw_url[2:]  # remove leading '--'
             cleaned_url = substitute_hex(cleaned_url)
             if "apivtwo" in cleaned_url:
@@ -181,7 +197,11 @@ def fetch_usable_urls(data):
     if not usable_urls:
         return []
 
+    print("usable_urls  ", usable_urls)
+
     def priority(link):
+        if not link:
+            return 5
         if "myanime.sharepoint.com" in link:
             return 0
         elif "1080p" in link:
@@ -193,43 +213,51 @@ def fetch_usable_urls(data):
         else:
             return 4
 
-    usable_urls = sorted(usable_urls, key=priority)
-    return {'urls': usable_urls}
+    usable_urls = [i for i in usable_urls if i]
 
-@app.route('/anime/<anime_name>/<anime_id>/episode_data/<ep_number>/play/dub')
+    usable_urls = sorted(usable_urls, key=priority)
+    return {"urls": usable_urls}
+
+
+@app.route("/anime/<anime_name>/<anime_id>/episode_data/<ep_number>/play/dub")
 def get_ep_data_dub(anime_name, anime_id, ep_number):
-    video_data = get_episode_data(anime_name, anime_id, ep_number, lang='dub')
+    video_data = get_episode_data(anime_name, anime_id, ep_number, lang="dub")
     return video_data
 
-def get_episode_data(anime_name, anime_id, ep_number, lang='sub'):
-    usable_urls = fetch_usable_urls(data={'anime_id': anime_id, 'ep_number': ep_number, 'lang': lang}).get('urls', [])
+
+def get_episode_data(anime_name, anime_id, ep_number, lang="sub"):
+    usable_urls = fetch_usable_urls(
+        data={"anime_id": anime_id, "ep_number": ep_number, "lang": lang}
+    ).get("urls", [])
     selected_source = "No usable URL found"
     if len(usable_urls) > 0:
         for _url in usable_urls:
-            if not _url.endswith('.m3u8'):
+            if not _url.endswith(".m3u8"):
                 selected_source = _url
                 break
 
     video_data = {
-        'video_url': selected_source,
-        'episode_number': ep_number,
-        'player_title': f"{anime_name} - Episode {ep_number} (Sub)",
-        'anime_name': anime_name
+        "video_url": selected_source,
+        "episode_number": ep_number,
+        "player_title": f"{anime_name} - Episode {ep_number} (Sub)",
+        "anime_name": anime_name,
     }
     return video_data
 
-@app.route('/anime/<anime_name>/<anime_id>/episode_data/<ep_number>/play')
+
+@app.route("/anime/<anime_name>/<anime_id>/episode_data/<ep_number>/play")
 def get_ep_data_sub(anime_name, anime_id, ep_number):
     video_data = get_episode_data(anime_name, anime_id, ep_number)
     return video_data
 
-@app.route('/api/anime/episode/play', methods=['POST'])
+
+@app.route("/api/anime/episode/play", methods=["POST"])
 def anime_episode_play():
     data = request.json
     return fetch_usable_urls(data)
 
 
-@app.route('/anime/<anime_id>')
+@app.route("/anime/<anime_id>")
 def anime_detail(anime_id):
     query = """
     query ($showId: String!) {
@@ -253,20 +281,49 @@ def anime_detail(anime_id):
     episode_data = show.get("availableEpisodesDetail", {})
     episodes = sorted(episode_data.get("sub", []), key=lambda x: float(x))
 
-    return render_template("episodes.html", anime_id=anime_id, name=name, episodes=episodes)
+    return render_template(
+        "episodes.html", anime_id=anime_id, name=name, episodes=episodes
+    )
 
 
 def substitute_hex(input_str):
-    subs = {'01': '9', '08': '0', '05': '=', '0a': '2', '0b': '3', '0c': '4', '07': '?', '00': '8', '5c': 'd',
-            '0f': '7', '5e': 'f', '17': '/', '54': 'l', '09': '1', '48': 'p', '4f': 'w', '0e': '6', '5b': 'c',
-            '5d': 'e', '0d': '5', '53': 'k', '1e': '&', '5a': 'b', '59': 'a', '4a': 'r', '4c': 't', '4e': 'v',
-            '57': 'o', '51': 'i', }
+    subs = {
+        "01": "9",
+        "08": "0",
+        "05": "=",
+        "0a": "2",
+        "0b": "3",
+        "0c": "4",
+        "07": "?",
+        "00": "8",
+        "5c": "d",
+        "0f": "7",
+        "5e": "f",
+        "17": "/",
+        "54": "l",
+        "09": "1",
+        "48": "p",
+        "4f": "w",
+        "0e": "6",
+        "5b": "c",
+        "5d": "e",
+        "0d": "5",
+        "53": "k",
+        "1e": "&",
+        "5a": "b",
+        "59": "a",
+        "4a": "r",
+        "4c": "t",
+        "4e": "v",
+        "57": "o",
+        "51": "i",
+    }
 
     # Split input string into 2-character hex pairs
-    pairs = [input_str[i:i + 2] for i in range(0, len(input_str), 2)]
+    pairs = [input_str[i : i + 2] for i in range(0, len(input_str), 2)]
 
     # Apply substitutions
-    result = ''.join(subs.get(pair, chr(int(pair, 16))) for pair in pairs)
+    result = "".join(subs.get(pair, chr(int(pair, 16))) for pair in pairs)
     if "clock" in result:
         result = result.replace("clock", "clock.json")
         result = f"https://allanime.day{result}"
@@ -274,35 +331,42 @@ def substitute_hex(input_str):
     return result
 
 
-def play_episode_online_with_name(anime_name, anime_id, ep_number, lang='sub'):
-    usable_urls = fetch_usable_urls(data={'anime_id': anime_id, 'ep_number': ep_number, 'lang': lang}).get('urls', [])
+def play_episode_online_with_name(anime_name, anime_id, ep_number, lang="sub"):
+    usable_urls = fetch_usable_urls(
+        data={"anime_id": anime_id, "ep_number": ep_number, "lang": lang}
+    ).get("urls", [])
 
     # If only one .m3u8 URL, redirect to external player
     non_m3u8_url = False
     if len(usable_urls) > 0:
         for _url in usable_urls:
-            if not _url.endswith('.m3u8'):
+            if not _url.endswith(".m3u8"):
                 selected_source = _url
                 non_m3u8_url = True
                 break
     if not non_m3u8_url:
-        encoded_url = urllib.parse.quote(usable_urls[0], safe='')
+        encoded_url = urllib.parse.quote(usable_urls[0], safe="")
         redirect_url = f"https://allanime.day/player?url={encoded_url}"
         return redirect(redirect_url)
 
-    return render_template("video_player.html", video_url=selected_source, episode_number=ep_number,
-                           player_title=f"{anime_name}", anime_name=anime_name)
+    return render_template(
+        "video_player.html",
+        video_url=selected_source,
+        episode_number=ep_number,
+        player_title=f"{anime_name}",
+        anime_name=anime_name,
+    )
 
 
-@app.route('/anime/<anime_name>/<anime_id>/episode/<ep_number>/play/dub')
+@app.route("/anime/<anime_name>/<anime_id>/episode/<ep_number>/play/dub")
 def play_dub(anime_name, anime_id, ep_number):
-    return play_episode_online_with_name(anime_name, anime_id, ep_number, lang='dub')
+    return play_episode_online_with_name(anime_name, anime_id, ep_number, lang="dub")
 
 
-@app.route('/anime/<anime_name>/<anime_id>/episode/<ep_number>/play')
+@app.route("/anime/<anime_name>/<anime_id>/episode/<ep_number>/play")
 def play_sub(anime_name, anime_id, ep_number):
     return play_episode_online_with_name(anime_name, anime_id, ep_number)
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=10000)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=10000)
